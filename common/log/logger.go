6a78dd70bd9560d895f310982dc823758f2d0c54
CelestialMelody <celestialmelodygo@gmail.com>
2022-05-20 22:15:34 +0800

    logger基本配置；clstilmldy-dev

--- common/log/logger.go
+++ common/log/logger.go
@@ -1,4 +1,118 @@
 package log
 
+import (
+	"fmt"
+	"github.com/natefinch/lumberjack"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"tikapp/common/config"
+	"time"
+)
 
+type ZapLevel int
 
+// Logger 整个项目的Logger
+var Logger *zap.Logger
+
+// NameSpace 提供带有模块命名空间的logger
+// eg. logger.NameSpace("api").Error("failed to ...")
+func NameSpace(name string) *zap.SugaredLogger {
+	return zap.S().Named(name)
+}
+
+func Init() {
+	// 调试级别
+	debugPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.DebugLevel
+	})
+	// 日志级别
+	infoPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.InfoLevel
+	})
+	// 警告级别
+	warnPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.WarnLevel
+	})
+	// 错误级别
+	errorPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.ErrorLevel
+	})
+	// panic级别
+	panicPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.PanicLevel
+	})
+	// fatal级别
+	fatalPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.FatalLevel
+	})
+
+	cores := [...]zapcore.Core{
+		getEncoderCore(fmt.Sprintf("./debug.log"), debugPriority),
+		getEncoderCore(fmt.Sprintf("./info.log"), infoPriority),
+		getEncoderCore(fmt.Sprintf("./warn.log"), warnPriority),
+		getEncoderCore(fmt.Sprintf("./error.log"), errorPriority),
+		getEncoderCore(fmt.Sprintf("./panic.log"), panicPriority),
+		getEncoderCore(fmt.Sprintf("./fatal.log"), fatalPriority),
+	}
+
+	// zap.AddCaller() 可以获取到文件名和行号
+	Logger = zap.New(zapcore.NewTee(cores[:]...), zap.AddCaller())
+}
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+func getLogWriter(fileName string) zapcore.WriteSyncer {
+	dir, _ := os.Getwd() // 获取项目目录
+	// 	log 存放路径
+	dir = dir + "/runtime/logs"
+	if !pathExists(dir) {
+		_ = os.Mkdir(dir, os.ModePerm)
+		logrus.Warn("create dir %s failed", dir)
+	}
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   dir + "/" + fileName, // 日志文件路径
+		MaxSize:    5,                    // 设置日志文件最大尺寸
+		MaxBackups: 5,                    // 设置日志文件最多保存多少个备份
+		MaxAge:     30,                   // 设置日志文件最多保存多少天
+		Compress:   true,                 // 是否压缩 disabled by default
+	}
+	// 返回同步方式写入日志文件的zapcore.WriteSyncer
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+func getEncoderCore(fileName string, level zapcore.LevelEnabler) (core zapcore.Core) {
+	writer := getLogWriter(fileName) // 使用file-rotatelogs进行日志分割
+	return zapcore.NewCore(getEncoder(), writer, level)
+}
+
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(getEncoderConfig())
+}
+
+func getEncoderConfig() (config zapcore.EncoderConfig) {
+	config = zapcore.EncoderConfig{
+		MessageKey:     "message",
+		LevelKey:       "level",
+		TimeKey:        "time",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder, // 将日志级别字符串转化为小写
+		EncodeTime:     CustomTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder, // 执行消耗时间转化成浮点型的秒
+		EncodeCaller:   zapcore.ShortCallerEncoder,     // 以包/文件:行号 格式化调用堆栈
+	}
+	config.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	return config
+}
+
+// CustomTimeEncoder 自定义日志输出时间格式
+func CustomTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format(config.LogCfg.TimeFormat))
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ go 1.18
 replace tikapp => ./
 
 require (
+	github.com/astaxie/beego v1.12.3
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.5.4
 	github.com/gin-gonic/gin v1.7.4
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.11.0
+	go.uber.org/zap v1.21.0
 	gorm.io/driver/mysql v1.3.3
 	gorm.io/gorm v1.23.5
 )
@@ -33,17 +36,21 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )

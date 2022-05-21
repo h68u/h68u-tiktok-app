82bfe3b76d7b4964067b58b4a185c763507c6c13
sjcsjc123 <1401189096@qq.com>
2022-05-21 14:46:57 +0800

    update

---
+++ common/model/user.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+	Id            int64  `gorm:"primaryKey"`
+	Name          string `gorm:"index"`
+	Username      string
+	Password      string
+	FollowCount   int64
+	FollowerCount int64
+	IsFollow      bool
+}

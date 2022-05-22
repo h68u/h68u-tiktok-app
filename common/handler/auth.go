0dd71e106e85e278d37457b27254e13b7cb99d6c
sjcsjc123 <1401189096@qq.com>
2022-05-22 11:20:14 +0800

    鉴权

---
+++ common/handler/auth.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"tikapp/common/db"
+	"tikapp/common/log"
+	"tikapp/util"
+)
+
+var logger = log.NameSpace("auth")
+
+//鉴权接口
+func Auth() gin.HandlerFunc {
+	//先判断请求头是否为空，为空则为游客状态
+	return func(c *gin.Context) {
+		token := c.GetHeader("token")
+		if token == "" {
+			//判断浏览器是否存在cookie，存在表示非第一次访问
+			logger.Info("start valid cookie")
+			_, err := c.Cookie("visit-user")
+			if err != nil {
+				//没有这个cookie，第一次访问
+				logger.Info("first visit")
+				//生成唯一id，作为游客的userId
+				u := uuid.New()
+				//TODO cookie有效期待定
+				c.SetCookie("visit-user", u.String(), 30*24*60*60*1000, "/", "localhost", false, true)
+				c.Next()
+				return
+			}
+			//有cookie,直接next
+			c.Next()
+			return
+		}
+		//有token，判断是否过期
+		validToken, err := util.ValidToken(token)
+		if err != nil || validToken {
+			//token过期或者解析token发生错误
+			logger.Info("token expire or parse token error")
+			logger.Info("valid refreshToken")
+			refreshToken := db.Redis.Get(token)
+			b, err1 := util.ValidToken(refreshToken.String())
+			if err1 != nil || b {
+				//refreshToken出问题，表明用户三十天未登录，需要重新登录
+				logger.Info("user need login again")
+				//直接变成访客状态
+				u := uuid.New()
+				c.SetCookie("visit-user", u.String(), 30*24*60*60*1000, "/", "localhost", false, true)
+				c.Next()
+				return
+			}
+			//根据refreshToken刷新accessToken
+			userId, err1 := util.GetUsernameFormToken(refreshToken.String())
+			if err1 != nil {
+				logger.Error("parse token error")
+				//token解析不了的情况一般很少,暂时panic一下
+				panic(err1)
+			}
+			accessToken, err1 := util.CreateAccessToken(userId)
+			if err1 != nil {
+				logger.Error("parse token error")
+				//token解析不了的情况一般很少,暂时panic一下
+				panic(err1)
+			}
+			c.Header("token", accessToken)
+			c.Next()
+			return
+		}
+	}
+
+}

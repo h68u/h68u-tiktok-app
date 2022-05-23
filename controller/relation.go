251948528a57f7dde18d704fc40153bd3ba3929b
sslime336 <97684920+sslime336@users.noreply.github.com>
2022-05-24 00:32:33 +0800

    fix: add files

---
+++ controller/relation.go
@@ -0,0 +1,20 @@
+package ctrl
+
+import "github.com/gin-gonic/gin"
+
+
+// RelationAction 关注或取消关注
+func RelationAction(c *gin.Context) {
+
+}
+
+// FollowList 获取用户关注的列表
+func FollowList(c *gin.Context) {
+
+}
+
+// FollowerList 获取用户的粉丝列表
+func FollowerList(c *gin.Context) {
+
+}
+

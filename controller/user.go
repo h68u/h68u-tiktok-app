00545d723968731d82f9884476f79d8e3b24ce87
youtiao223 <1946098283@qq.com>
2022-05-24 21:20:24 +0800

    feat: add user register function

--- controller/user.go
+++ controller/user.go
@@ -8,7 +8,28 @@ import (
 
 // Register 新用户注册
 func Register(c *gin.Context) {
-
+	var u srv.User
+	register, err := u.Register(c)
+	if err != nil {
+		// 用户名重复
+		if err == srv.ErrUsernameExits {
+			res.Error(c, res.Status{
+				StatusCode: res.UsernameExitErrorStatus.StatusCode,
+				StatusMsg:  res.UsernameExitErrorStatus.StatusMsg,
+			})
+		} else {
+			res.Error(c, res.Status{
+				StatusCode: res.RegisterErrorStatus.StatusCode,
+				StatusMsg:  res.RegisterErrorStatus.StatusMsg,
+			})
+		}
+		return
+	}
+	data := register.(srv.UserRegisterResp)
+	res.Success(c, res.R{
+		"userid": data.UserId,
+		"token":  data.Token,
+	})
 }
 
 // Login 用户登录

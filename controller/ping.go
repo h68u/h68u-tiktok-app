52d3a8b944d1e27056c2ad0e7c17c0d8e2bb03c1
sjcsjc123 <1401189096@qq.com>
2022-05-25 16:05:42 +0800

    oss

--- controller/ping.go
+++ controller/ping.go
@@ -1,7 +1,14 @@
 package ctrl
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func Ping(c *gin.Context) {
+	url := c.Request.URL
+	fmt.Println(url.String())
+	uri := c.Request.RequestURI
+	fmt.Println(uri)
 	c.JSON(200, gin.H{"status": "测试webhook!"})
 }

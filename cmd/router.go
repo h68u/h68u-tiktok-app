5d44515247724f4d92635300a31d6972f4511508
sjcsjc123 <1401189096@qq.com>
2022-05-25 10:22:52 +0800

    路由修改

--- cmd/router.go
+++ cmd/router.go
@@ -33,50 +33,50 @@ func handle(r *gin.Engine) {
 		userGroup.POST("/register/", ctrl.Register)
 
 		// 用户登录
-		userGroup.POST("/login", ctrl.Login)
+		userGroup.POST("/login/", ctrl.Login)
 	}
 
 	// 视频投稿相关
 	publishGroup := basic.Group("/publish")
 	{
 		// 用户上传视频
-		publishGroup.POST("/action", ctrl.PublishAction)
+		publishGroup.POST("/action/", ctrl.PublishAction)
 
 		// 直接列出用户投稿过的所有视频
-		publishGroup.GET("/list", ctrl.PublishList)
+		publishGroup.GET("/list/", ctrl.PublishList)
 	}
 
 	// 点赞相关
 	favoriteGroup := basic.Group("/favorite")
 	{
 		// 点赞 取消点赞
-		favoriteGroup.POST("/action", ctrl.FavoriteAction)
+		favoriteGroup.POST("/action/", ctrl.FavoriteAction)
 
 		// 获取点赞列表
-		favoriteGroup.GET("/list", ctrl.FavoriteList)
+		favoriteGroup.GET("/list/", ctrl.FavoriteList)
 	}
 
 	// 评论相关
 	commentGroup := basic.Group("/comment")
 	{
 		// 评论
-		commentGroup.POST("/action", ctrl.CommentAction)
+		commentGroup.POST("/action/", ctrl.CommentAction)
 
 		// 倒叙查看评论
-		commentGroup.GET("/list", ctrl.CommentList)
+		commentGroup.GET("/list/", ctrl.CommentList)
 	}
 
 	// 用户间关系操作 如关注 获取关注列表
 	relationGroup := basic.Group("/relation")
 	{
 		// 对指定用户 关注 取关
-		relationGroup.POST("/action", ctrl.RelationAction)
+		relationGroup.POST("/action/", ctrl.RelationAction)
 
 		// 获取用户的关注列表
-		relationGroup.GET("/follow/list", ctrl.FollowList)
+		relationGroup.GET("/follow/list/", ctrl.FollowList)
 
 		// 获取用户的粉丝列表
-		relationGroup.GET("/follower/list", ctrl.FollowerList)
+		relationGroup.GET("/follower/list/", ctrl.FollowerList)
 	}
 
 }

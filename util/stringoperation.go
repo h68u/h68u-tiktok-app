4708d1b03c5eafd3542e090a0f4984d855e6c505
zzh <1192643830@qq.com>
2022-05-31 11:32:17 +0800

    修改类型转换

--- util/stringoperation.go
+++ util/stringoperation.go
@@ -2,13 +2,14 @@ package util
 
 import (
 	"strings"
+	"strconv"
 )
 
 func Connect(str1 int64,str2 int64)(string){
 	var str strings.Builder
-	str.WriteString(string(str1))
+	str.WriteString(strconv.FormatInt(str1,10))
 	str.WriteString("::")
-	str.WriteString(string(str2))
+	str.WriteString(strconv.FormatInt(str2,10))
 	return str.String()
 
 }

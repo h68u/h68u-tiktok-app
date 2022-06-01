3a4d30e0d28d2647b98350e04abdb8634bc2e3bc
CelestialMelody <celestialmelodygo@gmail.com>
2022-06-01 20:01:25 +0800

    tidy:feed

--- service/feed.go
+++ service/feed.go
@@ -31,29 +31,38 @@ type UserDemo struct {
 	IsFollow      bool   `json:"is_follow"`
 }
 
+// Feed 获取视频列表
+// id 若为-1，表示没有获取到用户id
+// latestTime 本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
 func (f Feed) Feed(id int64) (interface{}, error) {
+	// 目前：取出最新视频
+	// TODO：控制视频数量
 	var videos []model.Video
 	err := db.MySQL.Model(&model.Video{}).Order("create_time desc").Find(&videos).Error
 	if err != nil {
 		return nil, err
 	}
-	var max int64 = 0
+
+	var latestTime int64 = 0
 	videoDemos := make([]VideoDemo, 0)
 	for _, v := range videos {
+		// 获取作者信息
 		authorId := v.PublishId
 		var u model.User
-		err1 := db.MySQL.Model(&model.User{}).Where("id = ?", authorId).First(&u).Error
-		if err1 != nil {
-			return nil, err1
+		err := db.MySQL.Model(&model.User{}).Where("id = ?", authorId).First(&u).Error
+		if err != nil {
+			return nil, err
 		}
+
+		// 用户是否关注了
 		var isFollow bool
 		if id == -1 {
 			isFollow = false
 		} else {
 			var count int64
-			err2 := db.MySQL.Model(&model.Follow{}).Where("user_id = ? and follow_id = ?", authorId, id).Count(&count).Error
-			if err2 != nil {
-				return nil, err2
+			err := db.MySQL.Model(&model.Follow{}).Where("user_id = ? and follow_id = ?", authorId, id).Count(&count).Error
+			if err != nil {
+				return nil, err
 			}
 			if count == 0 {
 				isFollow = false
@@ -61,21 +70,24 @@ func (f Feed) Feed(id int64) (interface{}, error) {
 				isFollow = true
 			}
 		}
+
+		// 视频作者基本信息
 		userDemo := UserDemo{
 			Id:            authorId,
 			Name:          u.Name,
 			FollowCount:   u.FollowCount,
 			FollowerCount: u.FollowerCount,
-			IsFollow:      isFollow,
+			IsFollow:      isFollow, // 用户是否关注了作者
 		}
+		// 用户是否点赞了
 		var isFavorite bool
 		if id == -1 {
 			isFavorite = false
 		} else {
 			var count int64
-			err2 := db.MySQL.Model(&model.VideoFavorite{}).Where("user_id = ? and video_id = ?", id, v.Id).Count(&count).Error
-			if err2 != nil {
-				return nil, err2
+			err := db.MySQL.Model(&model.VideoFavorite{}).Where("user_id = ? and video_id = ?", id, v.Id).Count(&count).Error
+			if err != nil {
+				return nil, err
 			}
 			if count == 0 {
 				isFavorite = false
@@ -83,6 +95,8 @@ func (f Feed) Feed(id int64) (interface{}, error) {
 				isFavorite = true
 			}
 		}
+
+		// 视频信息
 		videoDemos = append(videoDemos, VideoDemo{
 			Id:            v.Id,
 			Author:        userDemo,
@@ -93,12 +107,13 @@ func (f Feed) Feed(id int64) (interface{}, error) {
 			IsFavorite:    isFavorite,
 			Title:         v.Title,
 		})
-		if max < v.CreateTime {
-			max = v.CreateTime
+		if latestTime < v.CreateTime {
+			latestTime = v.CreateTime
 		}
 	}
+
 	resp := FeedResp{
-		NextTime:  max,
+		NextTime:  latestTime,
 		VideoList: videoDemos,
 	}
 	return resp, nil

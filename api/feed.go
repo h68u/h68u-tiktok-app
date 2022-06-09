d58ab18ba1c34f31c14f5e874e78c825cc27541e
sslime336 <97684920+sslime336@users.noreply.github.com>
2022-06-09 15:31:58 +0800

    del: api module

--- api/feed.go
+++
@@ -1,5 +0,0 @@
-package api
-
-type FeedHandler interface {
-
-}
--- api/user.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "github.com/gin-gonic/gin"
-
-type UserHandler interface {
-
-	//查询userid，并由此创建jwt token
-	Login(c *gin.Context) (interface{}, error)
-}
